7b18047623bafc0c47d5f6101c36413df0e999c7
blt2114 <blt2114@columbia.edu>
2015-02-01 10:55:45 -0800

    /genMap/ now actually generates JSON

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ import random
 @bottle.get("/genMap/")
 def retrunScreen():
         #generate 20 random tiles
+        screen=[]
         for i in range (1,20):
                 num1=random.randint(0,19);
                 num2=random.randint(0,19);
-                tile = {'x':num1,'y':num2,'type':{}}
-                screen.append(tile) 
-                print screen
-                return  screen
+                #tile = {'x':num1,'y':num2,'type':{}}
+                screen.append({'x':num1,'y':num2,'type':{}}) 
+        return  {"screen":screen}
 bottle.debug(True)
 bottle.run(host='localhost', port=8080)
 
