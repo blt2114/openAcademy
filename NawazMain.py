db81c54b271b1ec1956fb6bed18f83e161a5f696
Riaz1729 <rah2181@columbia.edu>
2015-02-01 17:16:08 -0500

    Update and rename NiteshMain.py to NawazMain.py

---
+++ NawazMain.py
@@ -0,0 +1,33 @@
+import pymongo
+import bottle #micro-framework
+import sys
+from random import randint
+from bottle import route, run, template
+def generate_tiles():
+    #generate 20 random tiles
+    tiles=[]
+    for x in range(20):
+        for y in range(20):
+            c = randint(1,6)
+            if c >= 5:
+                tiles.append({'x':x,'y':y})
+    return tiles 
+
+#@bottle.get('/login'):
+connection = pymongo.Connection("mongodb://localhost", safe = True)
+db = connection.test
+world = db.world
+users = db.users
+db.users.insert({'x': 0, 'y': 0})
+
+@bottle.get("/generate_screen")
+def build_screen():
+    screen1 = {}
+    screen1['tiles'] = generate_tiles()
+    screen1['users'] = [{'x': 0, 'y':0}]
+    db.world.insert(screen1)
+    screen1.pop('_id', None)
+    return {'screen':screen1}
+
+bottle.debug(True)
+bottle.run(host='localhost', port=8080)
