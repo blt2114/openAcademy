d5c1012bbe163e4d48bc95be5814caebb9737781
Riaz1729 <rah2181@columbia.edu>
2015-02-01 17:20:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,34 @@
 import pymongo
 import bottle #micro-framework
 import sys
-import random
+from random import randint
+from bottle import route, run, template
+def generate_tiles():
+    #generate 20 random tiles
+    tiles=[]
+    for x in range(20):
+        for y in range(20):
+            c = randint(1,6)
+            if c >= 5:
+                tiles.append({'x':x,'y':y})
+    return tiles 
 
+#@bottle.get('/login'):
+connection = pymongo.Connection("mongodb://localhost", safe = True)
+db = connection.test
+world = db.world
+users = db.users
+db.users.insert({'x': 0, 'y': 0})
 
-#route to get the screen rendering
-@bottle.get("/genMap/")
-def retrunScreen():
-        #generate 20 random tiles
-        screen=[]
-        for i in range (1,20):
-                num1=random.randint(0,19);
-                num2=random.randint(0,19);
-                #tile = {'x':num1,'y':num2,'type':{}}
-                screen.append({'x':num1,'y':num2,'type':{}}) 
-        return  {"screen":screen}
+@bottle.get("/generate_screen")
+def build_screen():
+    screen1 = {}
+    screen1['tiles'] = generate_tiles()
+    screen1['users'] = [{'x': 0, 'y':0}]
+    db.world.insert(screen1)
+    screen1.pop('_id', None)
+    return {'screen':screen1}
+
 bottle.debug(True)
 bottle.run(host='localhost', port=8080)
 
