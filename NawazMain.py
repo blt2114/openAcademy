9812077d591a7afe0ac76c78a7566e8d838cc222
blt2114 <blt2114@columbia.edu>
2015-02-07 14:02:26 -0500

    larger world, still buggy

--- NawazMain.py
+++ NawazMain.py
@@ -9,12 +9,12 @@ WORLD_LEN = 10 #number of screens per row and column of the world
 
 
 def generate_tiles():
-    #generate 20 random tiles
+    #generate tiles in random positions in 20X20 grid.  1/6 of positions
     tiles=[]
     for x in range(20):
         for y in range(20):
             c = randint(1,6)
-            if c >= 5:
+            if c > 5:
                 tiles.append({'x':x,'y':y})
     return tiles 
 
@@ -24,27 +24,50 @@ world = db.world
 users = db.users
 
 def create_user():
-    user = {'x': randint(10,15), 'y':randint(10,15)}
+    user = {'X':1,'Y':1,'x': randint(10,15), 'y':randint(10,15)}
     return user
+
+def create_world():
+    for X in range(WORLD_LEN):
+        for Y in range(WORLD_LEN):
+            screen = {}
+            screen['tiles'] = generate_tiles()
+            screen['X']=X
+            screen['Y']=Y
+            screen['users']=[]
+            if world.find({'X':X,'Y':Y}).count():
+                sys.stderr.write("screen in this position already exists.")
+                continue
+            world.insert(screen)
+
+def get_screen(user):
+    X=user['X']
+    Y=user['Y']
+    cur = world.find({"X":X,"Y":Y})
+    if not cur.count():
+        sys.stderr.write("No screen in found in current position")
+        return None
+    return cur[0]
 
 @bottle.get("/load_screen")
 def load_screen():
     screen1 = {}
-    #print "loading screen"
     if (world.count()==0):
-        screen1['tiles'] = generate_tiles()
+        create_world()
         user1 = create_user()
+        print "about to insert user"+str(user1)
         users.insert(user1)
-        print "inserted user into user db"
-        if user1 in screen1['tiles']:
-            screen1['tiles'].pop(user1)
-        screen1['users'] = [user1]
-        world.insert(screen1)
-        print "inserted screen into world db"
-    #else:
-    #    print "data base already has a user"
-    screen = world.find_one()
-    print screen['users']
+        print "just inserted user:"+str(user1)
+        screen = get_screen(user1)
+        users=screen['users']
+        users.append(user)
+        screen['users']=users
+        world.update({"_id":screen['_id']},{"$set":screen})
+      # ''' if user1 in screen1['tiles']:
+       #     screen1['tiles'].pop(user1)'''
+    #in future this can find the user based on a cookie passed in
+    current_user=find_user()
+    screen = get_screen(current_user)
     screen.pop('_id', None)
     for user in screen["users"]:
         user.pop('_id',None)
@@ -64,35 +87,69 @@ AXES = {"up":'y',"down":'y',"left":'x',"right":'x'}
 DIRECTIONS = {"up":-1,"down":1,"left":-1,"right":1}
 MOVE_DIR = {"up":(0,-1),"down":(0,1),"left":(-1,0),"right":(1,0)}
 def update_position(user,move):
-    print "updating position"
-    uid=user["_id"]
-    users.update({"_id": uid}, {"$inc" : {AXES[move] : DIRECTIONS[move]}})
-    world.update({"users":{"$elemMatch":{"_id":uid}}},{"$inc":{'users.$.'+AXES[move]:DIRECTIONS[move]}})
+    screen = get_screen(user)
+    new_pos = new_coord(user,move)
+    user['Y']=new_pos['Y']
+    user['y']=new_pos['y']
+    user['X']=new_pos['X']
+    user['x']=new_pos['x']
+    if ((new_pos['X']==user['X'] )& (new_pos['Y']==user['Y'])):
+        for u in screen['users']:
+            if u["_id"]==user["_id"]:
+                u['x']=new_pos['x']
+                u['y']=new_pos['y']
+                break
+        world.update({"_id":screen["_id"]},{"$set":{"users":screen["users"]}})
+    else:
+        for u in screen['users']:
+            if u["_id"]==user["_id"]:
+                screen['users'].remove(u) 
+                world.update({"_id":screen['_id']},{"$set":screen})
+                break
+        new_screen = get_screen(new_pos)
+        new_screen['users'].append(user)
+        world.update({"_id":new_screen['_id']},{"$set":new_screen})
+    users.update({"_id": user["_id"]}, {"$set" : user})
+    #world.update({"users":{"$elemMatch":{"_id":uid}}},{"$inc":{'users.$.'+AXES[move]:DIRECTIONS[move]}})
 
 '''#TODO update this to return new poosition including screen position and
 relative position'''
-def new_coord(pos,move):
-    return (pos[0]+MOVE_DIR[move][0],pos[1]+MOVE_DIR[move][1])
+def new_coord(user,move):
+    X=user['X']
+    Y=user['Y']
+    x=user['x']
+    y=user['y']
+    dx = MOVE_DIR[move][0]
+    dy = MOVE_DIR[move][1]
+    
+    if (x+dx)<0:
+        X = X-1
+    elif  (x+dx)>=SCREEN_LEN:
+        X = X+1
+    x = (x+dx)%SCREEN_LEN
+    if (y+dy)<0:
+        Y = Y-1
+    elif (y+dy)>=SCREEN_LEN:
+        Y = Y+1
+    y = (y+dy)%SCREEN_LEN
+    return {"X":X,"Y":Y,"x":x,"y":y}
 
 #TODO: update to check in corrects screen
 def terrain_at(pos):
-    tile = world.find({"tiles":{"x":pos[0],"y":pos[1]}})
+    screen = get_screen(pos)
+    screen = world.find({"X":pos["X"],"Y":pos["Y"]})
+    if not screen.count():
+        return True
+    tile = world.find({"X":pos["X"],"Y":pos["Y"],"tiles":{"x":pos['x'],"y":pos['y']}})
     if tile.count():
         return True
-#    print "tile"
-#    for doc in tile: 
-#        print doc
     return False
 
 '''#TODO: potentially add functionality allow different users to move on
 different terrain types '''
 def can_move(user,move):
-    x = user["x"]
-    y = user["y"]
-    new_pos = new_coord((x,y),move)
-    print "new position is:"
-    print new_pos[0]
-    print new_pos[1]
+    new_pos = new_coord(user,move)
+    print "new position is:"+str(new_pos)
     return not terrain_at(new_pos)
 
 
@@ -108,10 +165,12 @@ def postResource():
         sys.stderr.write("User Not Found!")
         return
     uid =user["_id"]
+    X = user["X"]
+    Y = user["Y"]
     x = user["x"]
     y = user["y"]
 
-    screen = world.find_one()
+    screen = get_screen(user)
     move = bottle.request.json['move']
     if not move in AXES:
         return
@@ -120,30 +179,6 @@ def postResource():
         update_position(user,move)
     else:
         print "invalid move"
-    '''
-        user = users.find_one()
-        uid =user["_id"]
-        x = user["x"]
-        y = user["y"]
-
-        screen = world.find_one()
-        direction = bottle.request.json['move']
-
-        if direction == "up":
-                users.update({"_id": uid}, {"$inc" : {"y" : -1}})
-                world.update({"users":{"$elemMatch":{"_id":uid}}},{"$inc":{'users.$.y':-1}});
-        elif direction == "down":
-                users.update({"_id": uid}, {"$inc" : {"y" : 1}})
-                world.update({"users":{"$elemMatch":{"_id":uid}}},{"$inc":{'users.$.y':1}});
-        elif direction == "left":
-                users.update({"_id": uid}, {"$inc" : {"x" : -1}})
-                world.update({"users":{"$elemMatch":{"_id":uid}}},{"$inc":{'users.$.x':-1}});
-        elif direction == "right":
-                users.update({"_id": uid}, {"$inc" : {"x" : 1}})
-                world.update({"users":{"$elemMatch":{"_id":uid}}},{"$inc":{'users.$.x':1}});
-        return
-        '''
-
 
 @bottle.get('/<filename>')
 def serve_index(filename):
